da7c0e711e1dd5706c6d9d41e2f38c91e6d7f3ed
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-20 12:43:38 -0400

    add facade spec

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -6,7 +6,7 @@ class SearchFacade
   def parks
     service = ParkService.new
     json = service.parks_by_state(@state)
-    
+
     @parks = json[:data].map do |park_data|
       Park.new(park_data)
     end
--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
 RSpec.describe SearchFacade do
-  
+  describe "#parks" do
+    it "can make park objects" do
+      parks_data = SearchFacade.new("TN").parks
+
+      expect(parks_data).to be_an(Array)
+      expect(parks_data.count).to eq(15)
+
+      parks_data.each do |park|
+        expect(park).to be_a(Park)
+        expect(park.description).to be_a(String)
+        expect(park.directions_info).to be_a(String)
+        expect(park.full_name).to be_a(String)
+        expect(park.standard_hours).to be_a(Hash)
+      end
+
+    end
+  end
 end
